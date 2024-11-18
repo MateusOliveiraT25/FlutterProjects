dbf43e2421d473a2c58c77b5bb50de7a159b394b
Mateus Oliveira <123783867+MateusOliveiraT25@users.noreply.github.com>
2024-11-18 10:25:46 -0300

    Update visitor_registration_screen.dart

--- condominio_app/lib/screens/visitor_registration_screen.dart
+++ condominio_app/lib/screens/visitor_registration_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:go_router/go_router.dart'; // Usando go_router para navegação
-import 'package:qr_flutter/qr_flutter.dart'; // Importando para gerar QR code
+import 'package:go_router/go_router.dart';
 
 class VisitorRegistrationScreen extends StatefulWidget {
   const VisitorRegistrationScreen({super.key});
@@ -16,7 +15,6 @@ class _VisitorRegistrationScreenState extends State<VisitorRegistrationScreen> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController documentController = TextEditingController();
 
-  // Função para salvar os dados temporários do visitante no Firestore
   Future<void> _saveVisitor() async {
     try {
       final currentUser = FirebaseAuth.instance.currentUser;
@@ -27,34 +25,30 @@ class _VisitorRegistrationScreenState extends State<VisitorRegistrationScreen> {
         return;
       }
 
-      // Dados do visitante a ser registrado temporariamente
       final visitorData = {
         'nome': nameController.text,
         'documento': documentController.text,
-        'horario_entrada': DateTime.now(), // Armazena o horário de entrada
-        'uid': currentUser.uid, // Armazena o UID do usuário logado
-        'status': 'pendente', // O status será "pendente" até aprovação do administrador
+        'horario_entrada': DateTime.now(),
+        'status': 'pendente', // Status pendente até ser aprovado
       };
 
-      // Salva os dados na coleção 'visitantes_pendentes'
+      // Salvar os dados no Firestore
       DocumentReference docRef = await FirebaseFirestore.instance
           .collection('visitantes_pendentes')
           .add(visitorData);
 
-      // Gerar o QR Code com os dados do visitante
+      // Navegar para a tela de geração de QR Code
       final visitorInfo = {
-        'id': docRef.id, // ID do documento gerado
+        'id': docRef.id,
         'nome': nameController.text,
         'documento': documentController.text,
       };
 
-      // Navegar para a tela de QR Code, passando os dados
       context.push(
-        '/generateQrCode', // Usa o go_router para navegação
-        extra: visitorInfo, // Passa os dados para a próxima tela
+        '/generateQrCode',
+        extra: visitorInfo,
       );
 
-      // Limpar os campos após o envio
       nameController.clear();
       documentController.clear();
 
